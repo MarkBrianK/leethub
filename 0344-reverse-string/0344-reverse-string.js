5deecaa850dbe9f8d8200bda4102ab117a7f5d10
Kariuki Karanja <kariukimarkbrian@gmail.com>
2025-05-17 13:18:43 +0300

    Time: 0 ms (100%), Space: 60.7 MB (68.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,6 +3,6 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    const reversed = s.reverse()
-    return reversed
+    return s.reverse()
+    
 };

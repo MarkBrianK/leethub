23f98ae7e1fa414d0cbfe3ee52a963bacfe507d3
Kariuki Karanja <kariukimarkbrian@gmail.com>
2025-05-20 17:59:43 +0300

    Time: 0 ms (100%), Space: 53.1 MB (80.88%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,15 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let arr = s.split(" ").filter(word => word.length > 0);
-    const lastElement = arr.pop()
-    let count = 0;
-
-    for (let i = 0; i<lastElement.length; i++){
-        count++
-    }
-
-    return count
-
+    return s.split(" ").filter(word => word.length > 0).pop().length;
     
 };
